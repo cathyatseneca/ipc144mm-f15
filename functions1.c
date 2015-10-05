9c05819980882d38722c199ce290ad2b535927f1
Catherine Leung <catherine.leung@senecacollege.ca>
2015-10-05 13:52:47 -0400

    functions1

---
+++ functions1.c
@@ -0,0 +1,16 @@
+int myFirstFunction(int a, int b);
+
+int main(void){
+	int x=5;
+	int y=6;
+	int z=myFirstFunction(x,y);
+	printf("x = %d\n",x);
+	printf("y = %d\n",y);
+	printf("z = %d\n",z);
+	return 0;
+}
+
+int myFirstFunction(int a, int b){
+	int rc=a+b;
+	return rc;
+}
