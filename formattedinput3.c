d3341c9a46527db9df48b053153d7b65c2e8f511
Catherine Leung <cathyatseneca@gmail.com>
2015-11-19 14:10:19 -0500

    reading formatted input

--- formattedinput3.c
+++ formattedinput3.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
  int  clear(void)
- {	int rc=0;
-         while (getchar() != '\n'){
-		rc++;
-          }
+ {	 
+   int rc=0;     
+   while (getchar() != '\n'){
+      rc++;
+   }
+   return rc;
  }
 int main(void){
    int a,b;
@@ -13,9 +15,10 @@ int main(void){
    printf("prompt: "); 
    rc = scanf("%d, %d, %c",&a,&b,&c);
    int rc2=clear();
-   while(rc != 3 || rc2 > 0){
+   while(rc != 3|| rc2!= 0|| !(c == 'a' ||  c == 'b')){
 	   printf("prompt: "); 
-   	   rc=scanf("%d, %d, %c",&a,&b,&c);
+   	rc=scanf("%d, %d, %c",&a,&b,&c);
+      rc2=clear();
    }
    printf("%d %d %c\n",a,b,c);
 }
