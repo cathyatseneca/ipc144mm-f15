45c4f4761950ce3cc30eb18f911fcf1821edd84d
Catherine Leung <cathyatseneca@gmail.com>
2015-11-19 15:13:03 -0500

    structs

--- struct1.c
+++ struct1.c
@@ -8,7 +8,24 @@ int main(void){
 	struct Dog mowgli;
 	strcpy(mowgli.name_,"Mowgli");
 	mowgli.age_=8;
-	struct Dog frodo;
-	strcpy(frodo.name_,"Frodo");
-	frodo.age_=6;
+
+	struct Dog allDogs[5];
+	strcpy(allDogs[0].name_,"Frodo");
+	allDogs[0].age_=6;
+
+	allDogs[1]=mowgli;
+
+}
+
+//In C, structs can be very big.  So, instead of passing
+//a copy of an entire struct into a function, you typically
+//pass in just its address.
+
+void printDog(const struct Dog* dogPtr){
+	printf("%s - %d\n", dogPtr->name_,dogPtr->age_);
+//dogPtr->name_ is the same as (*dogPtr).name_
+}
+
+void printInt(int* ptr){
+	printf("%d\n", *ptr);
 }
